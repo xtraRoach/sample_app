9cbc10ff0b9a5ac8746541f0bf1d0bd67810eb06
Friedrich Kriegsheim <fkriegsheim@gmail.com>
2015-02-11 21:48:27 +0100

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def hello
-    render text: 'hello!'
+    render text: 'this is a test'
   end
 end
