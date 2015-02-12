ed8d558c4037ee210e033182eb7c4b83a41e81d1
Friedrich Kriegsheim <fkriegsheim@gmail.com>
2015-02-12 18:09:47 +0100

    added automated testing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Fallback page-title
+  def full_title(page_title = '', second_argument='')
+    base_title = "Ruby on Rails Tutorial"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
