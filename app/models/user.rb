2090e74166780ee29e9732d2e9664e41a1f337e1
Friedrich Kriegsheim <fkriegsheim@gmail.com>
2015-02-16 08:54:16 +0100

    added extensive tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,24 @@
 class User < ActiveRecord::Base
+  #Properties
+  has_secure_password
+  #Callbacks
+  before_save { self.email = email.downcase }
+ 
  #Validations  
+ 
+  #eMail Validations
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,  presence: true,            
+                    length: { maximum: 255},   
+                    format: { with: VALID_EMAIL_REGEX},
+                    uniqueness: { case_sensitive: false }
+
+  #Name Validations
   validates :name,   presence: true, 
                     length: { maximum: 50 }
-                   
- VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
- validates :email,  presence: true, 
-                    length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true
+
+  #Password Validations
+  validates :password, length: { minimum: 6 }
                     
-  validates :password, presence: true,
-                      length: {minimum: 5}
-    
-  has_secure_password
-  #callbacks 
-  before_save {self.email = email.downcase}                    
                     
 end
