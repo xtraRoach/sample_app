42260a50d9b6f037bb08e3e79a8dfcb28b909e1a
Friedrich Kriegsheim <fkriegsheim@gmail.com>
2015-02-25 21:35:22 +0100

    fixed footer

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216074323) do
+ActiveRecord::Schema.define(version: 20150216072842) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
